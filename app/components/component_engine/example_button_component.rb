c68a2a83ac1c1f51e88a611ca3dcca691f7de069
JuicyFlash <tankedet@bk.ru>
2025-02-06 02:01:54 +0300

    Добавлен простой компонент кнопка добавлен require для view_component

--- app/components/component_engine/example_button_component.rb
+++ app/components/component_engine/example_button_component.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-module ComponentEngine
-  class ExampleButtonComponent < ViewComponent::Base
-    def initialize(title:)
-      @title = title
-    end
+class ComponentEngine::ExampleButtonComponent < ViewComponent::Base
+  def initialize(title:)
+    @title = title
   end
 end
--- lib/component_engine.rb
+++ lib/component_engine.rb
@@ -1,5 +1,6 @@
 require "component_engine/version"
 require "component_engine/engine"
+require "view_component"
 
 module ComponentEngine
   # Your code goes here...
